92b3565b9f5050f4f622205f3901adfe9a88f897
Ahza Mazola <ahzamazola@gmail.com>
2024-07-15 00:07:10 +0700

    commit task 2

--- TASK-2/task2.py
+++ TASK-2/task2.py
@@ -1,31 +1,78 @@
-import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.types import BigInteger, String, DateTime, Boolean, Float, Integer
 
-# Set up database connection details
-db_config = {
-    'host': 'localhost',
-    'dbname': 'mydb',
-    'user': 'postgres',
-    'password': 'admin',
-    'port': 5432
-}
+pd.set_option('display.max_columns', None)
 
-# Connect to the PostgreSQL database
-try:
-    connection = psycopg2.connect(**db_config)
-    cursor = connection.cursor()
-    print("Connected to the database.")
+# 1 & 2) Create a DataFrame from a Parquet file and Load the parquet file to a DataFrame with fastparquet
+def read_parquet():
+    df = pd.read_parquet("/root/INGESTION-DATA/dataset/yellow_tripdata_2023-01.parquet", engine='fastparquet')
 
-    # Specify the table name
-    table_name = 'data_csv'
+    return df
 
-    # Create a SQL query to count the rows
-    count_query = f'SELECT COUNT(*) FROM {table_name}'
+# 3) Clean the Yellow Trip dataset
+def cleaning(df):
+    df.dropna(inplace=True)
 
-    # Execute the query
-    cursor.execute(count_query)
-    row_count = cursor.fetchone()[0]
+    #cast data type
+    df['VendorID'] = df['VendorID'].astype('int8')
+    df['passenger_count'] = df['passenger_count'].astype('int8')
+    df['PULocationID'] = df['PULocationID'].astype('int8')
+    df['DOLocationID'] = df['DOLocationID'].astype('int8')
+    df['payment_type'] = df['payment_type'].astype('int8')
 
-    print(f'Total number of rows in {table_name}: {row_count}')
+    df['store_and_fwd_flag'] = df['store_and_fwd_flag'].replace(['N', 'Y'], [False, True])
+    df['store_and_fwd_flag'] = df['store_and_fwd_flag'].astype('boolean')
 
-except Exception as e:
-    print(f'An error occurred: {e}')
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+
+    #Rename columns with snake_case format
+    df = df.rename(columns={'VendorID': 'vendor_id', 'RatecodeID': 'rate_code_id', 'PULocationID': 'pu_location_id', 'DOLocationID': 'do_location_id'})
+    
+    return df
+
+def connect_database():
+    user = 'postgres'
+    password = 'admin'
+    host = 'localhost'
+    database = 'mydb'
+    port = 5432
+    
+    conn_string = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+    engine = create_engine(conn_string)
+
+    return engine
+
+# 4) Define the data type schema
+def to_sql(df, engine):
+    df_schema = {
+        'vendor_id': BigInteger,
+        'tpep_pickup_datetime': DateTime,
+        'tpep_dropoff_datetime': DateTime,
+        'passenger_count': BigInteger,
+        'trip_distance': Float,
+        'rate_code_id': Float,
+        'store_and_fwd_flag': Boolean,
+        'pu_location_id': BigInteger,
+        'do_location_id': BigInteger,
+        'payment_type': BigInteger,
+        'fare_amount': Float,
+        'extra': Float,
+        'mta_tax': Float,
+        'tip_amount': Float,
+        'tolls_amount': Float,
+        'improvement_surcharge': Float,
+        'total_amount': Float,
+        'congestion_surcharge': Float,
+        'airport_fee': Float
+    }
+
+    df.to_sql(name='parquet', con=engine, if_exists="replace", index=False, schema="public", dtype=df_schema, method=None, chunksize=5000)
+
+# 5) Ingest the Yellow Trip dataset to PostgreSQL
+if __name__ == "__main__":
+    df = read_parquet()
+    cleaned_df = cleaning(df)
+    conn = connect_database()
+    to_sql(cleaned_df, conn)
