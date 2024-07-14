c71cad73493de1305b035ada5b1b7d2446e01feb
Ahza Mazola <ahzamazola@gmail.com>
2024-07-14 18:34:09 +0700

    commit task 1

--- TASK-1/task1.py
+++ TASK-1/task1.py
@@ -1,6 +1,19 @@
+print("------------------------  TASK 1 ------------------------------------")
 import pandas as pd
-
 pd.set_option('display.max_columns', None)
 df = pd.read_csv("../dataset/sample.csv", sep=",")
 print("Print the first row")
 print(df.head(1))
+
+
+print("------------------------ TASK 2 -------------------------------------------")
+df= df.rename(columns={"VendorID":"vendor_id","RatecodeID":"ratecode_id",
+              "PULocationID":"pu_location_id","DOLocationID":"do_location_id"})
+print(df.dtypes)
+
+
+print("------------------------- TASK 3 ------------------------------------------")
+df_multiple_cols = df[["vendor_id", "passenger_count", "trip_distance", "payment_type", "fare_amount", "extra", "mta_tax", "tip_amount", 
+                       "tolls_amount", "improvement_surcharge", "total_amount", "congestion_surcharge"]]
+print("Selecting multiple columns")
+print(df_multiple_cols.sort_values(by= "passenger_count", ascending=False).head(10))
